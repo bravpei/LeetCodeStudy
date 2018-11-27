d095d39336b0763f2ec457e7207f8e84b210d9e3
liupei-gs <403664797@qq.com>
2018-11-27 11:39:37 +0800

    1

--- src/main/java/moon/star/MoveZeroes.scala
+++ src/main/java/moon/star/MoveZeroes.scala
@@ -4,16 +4,20 @@ class MoveZeroes {
   def moveZeroes(nums:Array[Int]):Array[Int]={
     var i=0
     var j=0
-    while(j<nums.length){
+    while(j<nums.length&&i<nums.length){
       while(j<nums.length&&nums(j)==0){
         j+=1
       }
       while(i<nums.length&&nums(i)!=0){
         i+=1
       }
-      val a=nums(i)
-      nums(i)=nums(j)
-      nums(j)=a
+      if(i<nums.length&&j<nums.length){
+        if(nums(j)!=0&&nums(i)==0&&j>i){
+          val a=nums(i)
+          nums(i)=nums(j)
+          nums(j)=a
+        }else j=i
+      }
     }
     nums
   }
