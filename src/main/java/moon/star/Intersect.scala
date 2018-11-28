04d57d3a66ff6665776c0496e458cf13695dedad
liupei-gs <403664797@qq.com>
2018-11-28 15:39:28 +0800

    3

--- src/main/java/moon/star/Intersect.scala
+++ src/main/java/moon/star/Intersect.scala
@@ -4,12 +4,23 @@ import scala.collection.mutable.ArrayBuffer
 
 class Intersect {
   def intersect(nums1:Array[Int],nums2:Array[Int]):Array[Int]={
-    var nums3=nums2.toBuffer
-    var nums:ArrayBuffer[Int]=new ArrayBuffer[Int]()
-    var boo=true
+   var map=Map[Int,Int]()
+    var arr=ArrayBuffer[Int]()
     for(i<-nums1.indices){
-
+      if(map.isDefinedAt(nums1(i))){
+        map.+=(nums1(i)->(map(nums1(i))+1))
+      }else{
+        map.+=(nums1(i)->1)
+      }
     }
-    nums.toArray
+    for(i<-nums2.indices){
+      if(map.isDefinedAt(nums2(i))){
+        if(map(nums2(i))>0){
+          arr.+=(nums2(i))
+          map.+=(nums2(i)->(map(nums2(i))-1))
+        }
+      }
+    }
+    arr.toArray
   }
 }
