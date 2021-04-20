a7c5027a0eebd5c71ab91a290e4cf14ad18851eb
liupei <403664797@qq.com>
2021-04-20 11:46:46 +0800

    2

---
+++ src/main/java/moon/java/movezeroes/Solution.java
@@ -0,0 +1,17 @@
+package moon.java.movezeroes;
+
+public class Solution {
+    public void moveZeroes(int[] nums) {
+        int length=nums.length;
+        for(int i=0;i<length;i++){
+            if(nums[i]==0){
+                for(int j=i;j<length-1;j++){
+                    nums[j]=nums[j+1];
+                }
+                nums[length-1]=0;
+                i--;
+                length--;
+            }
+        }
+    }
+}
