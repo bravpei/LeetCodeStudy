22302204d91dd42bfba0094104b6eb64528a43d3
liupei <403664797@qq.com>
2021-11-05 11:30:53 +0800

    修改项目结构

---
+++ primary-algorithm/src/main/java/moon/java/string/firstuniqchar/Solution.java
@@ -0,0 +1,23 @@
+package moon.java.string.firstuniqchar;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public int firstUniqChar(String s) {
+        Set<String> set=new HashSet<String>();
+        for(int i=0;i<s.length();i++){
+            int j;
+            for(j=i+1;j<s.length();j++){
+                if(s.charAt(i)==s.charAt(j)){
+                    set.add(String.valueOf(s.charAt(i)));
+                    break;
+                }
+            }
+            if(j>=s.length()&&!set.contains(String.valueOf(s.charAt(i)))){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
